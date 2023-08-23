1a95aa3c0481de65b3a372c05a2fcd68e6cdf5d7
HHsungmoon <119782897+HHsungmoon@users.noreply.github.com>
2023-08-23 12:43:29 +0900

    [Gold IV] Title: 트리의 지름, Time: 4 ms, Memory: 3552 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.cc
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.cc
@@ -7,51 +7,51 @@
 using namespace std;
 
 int N;
-vector<int> leaf_node;
 
-
-int DFS(vector<vector<pair<int, int>>> vec)
+int DFS(int start, vector<vector<pair<int, int>>> vec, int type)
 {
 	int answer = 0;
 
-	while (leaf_node.size() > 1)
+	stack<pair<int, int>> st;
+	st.push(make_pair(start, 0));
+	vector<int> visited(N + 1, 0);
+	visited[start] = 1;
+	while (st.empty() == false)
 	{
-		int start = leaf_node.back();
-		leaf_node.pop_back();
+		int node = st.top().first;
+		int len = st.top().second;
+		st.pop();
 
-		stack<pair<int,int>> st;
-		st.push(make_pair(start, 0));
-		vector<int> visited(N + 1, 0);
-		visited[start] = 1;
-		while (st.empty() == false)
+		for (int i = 0; i < vec[node].size(); i++)
 		{
-			int node = st.top().first;
-			int len = st.top().second;
-			st.pop();
-			
-			for (int i = 0; i < vec[node].size(); i++)
+			if (visited[vec[node][i].first] == 0)
 			{
-				if (visited[vec[node][i].first] == 0)
-				{
-					st.push(make_pair(vec[node][i].first, len + vec[node][i].second));
-					visited[vec[node][i].first] = len + vec[node][i].second;
-				}
-				
+				st.push(make_pair(vec[node][i].first, len + vec[node][i].second));
+				visited[vec[node][i].first] = len + vec[node][i].second;
 			}
 		}
-
-		int max_len = *max_element(visited.begin(), visited.end());
-		answer = max(max_len, answer);
 	}
 
+	if (type == 0)
+	{
+		answer = max_element(visited.begin(), visited.end()) - visited.begin();
+	}
+	else if (type == 1)
+	{
+		answer = *max_element(visited.begin(), visited.end());
+	}
 	return answer;
 }
 
 int main()
 {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	std::cout.tie(0);
+
 	cin >> N;
-	vector<vector<pair<int,int>>> vec(N+1);
-	
+	vector<vector<pair<int, int>>> vec(N + 1);
+
 	for (int i = 0; i < N - 1; i++)
 	{
 		int a, b, c;
@@ -60,14 +60,14 @@ int main()
 		vec[b].push_back(make_pair(a, c));
 	}
 
-	for (int i = 1; i < N + 1; i++)
+	if (N == 1)
 	{
-		if (vec[i].size() == 1)
-			leaf_node.push_back(i);
+		cout << 0;
+		return 0;
 	}
-	sort(leaf_node.begin(), leaf_node.end(), greater<int>());
 
-	cout << DFS(vec);
+	int endp = DFS(1, vec, 0);
+	std::cout << DFS(endp,vec,1);
 
 	return 0;
 }
