72d2c2923783206b352102ee757f47f1b0e2b1ae
HHsungmoon <119782897+HHsungmoon@users.noreply.github.com>
2023-09-23 17:20:14 +0900

    [Gold III] Title: 파일 합치기, Time: 656 ms, Memory: 3008 KB -BaekjoonHub

---
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int INF = 100000000;
+
+int T;
+int K;
+int input;
+vector<int> answer;
+vector<int> Sum;
+vector<vector<int>> dp;
+
+int REQ(int st, int end)
+{
+	if (st == end)
+	{
+		return 0;
+	}
+	if (dp[end][st] != INF)
+	{
+		return dp[end][st];
+	}
+
+
+	for (int i = st; i < end; i++)
+	{
+		int L = REQ(st, i);
+		int R = REQ(i + 1, end);
+
+		dp[end][st] = min(dp[end][st], L + R + (Sum[end+1] - Sum[st]));
+	}
+
+	return dp[end][st];
+}
+
+int main()
+{
+	cin >> T;
+
+	answer.resize(T);
+
+	for (int t = 0; t < T; t++)
+	{
+		cin >> K;
+		Sum.resize(K + 1); Sum[0] = 0;
+		dp.clear();
+		dp.resize(K);
+		for (int k = 0; k < K; k++)
+			dp[k].resize(K,INF);
+
+		for (int k = 0; k < K; k ++)
+		{
+			cin >> dp[0][k];
+			Sum[k+1] = Sum[k] + dp[0][k];
+		}
+
+		int ans = REQ(0, K - 1);
+		answer[t] = ans;
+	}
+
+	for (int i = 0; i < answer.size(); i++)
+	{
+		cout << answer[i] << endl;
+	}
+
+	return 0;
+}
