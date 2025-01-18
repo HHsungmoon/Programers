b3bb9b30f9baafc38d825cea472fa992967a773b
HHsungmoon <119782897+HHsungmoon@users.noreply.github.com>
2025-01-18 11:17:05 +0900

    [Gold V] Title: 토마토, Time: 1480 ms, Memory: 321636 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class Point implements Comparable<Point>{
+class Point {
     public int x;
     public int y;
     public int cnt; 
@@ -10,11 +10,6 @@ class Point implements Comparable<Point>{
         this.x = x;
         this.y = y;
         this.cnt = cnt;
-    }
-
-    @Override
-    public int compareTo(Point other) {
-        return Integer.compare(this.cnt, other.cnt); // cnt 오름차순
     }
 }
 
@@ -29,12 +24,11 @@ class Main {
         // 입력 데이터를 읽기 위해 input 함수를 호출
         int[][] box = input(scanner, N, M);
 
-        PriorityQueue<Point> stque = new PriorityQueue<>();
+        Queue<Point> stque = new LinkedList<>();
         for(int y=0; y<N; y++){
             for(int x=0; x<M; x++){
                 if(box[y][x] == 1){
-                    Point tmp = new Point(x,y,1);
-                    stque.add(tmp);
+                    stque.add(new Point(x, y, 1));
                 }
             }
         }
@@ -53,10 +47,10 @@ class Main {
             }
         }
 
-        if(tf==false) 
-            System.out.println(""+(-1));
+        if(!tf) 
+            System.out.println(-1);
         else
-            System.out.println(""+(max_num-1));
+            System.out.println(max_num - 1);
     }
 
     public static int[][] input(Scanner scanner, int N, int M) {
@@ -73,23 +67,23 @@ class Main {
         return box; // 입력된 2차원 배열 반환
     }
 
-    public static int[][] BFS(PriorityQueue<Point> stque,int[][] box, int N, int M){
-        int[] dirx = {1,-1,0,0};
-        int[] diry = {0,0,1,-1};
+    public static int[][] BFS(Queue<Point> stque, int[][] box, int N, int M){
+        int[] dirx = {1, -1, 0, 0};
+        int[] diry = {0, 0, 1, -1};
         
         while(!stque.isEmpty()){
-            Point tp = stque.element();
+            Point tp = stque.poll(); // poll로 큐에서 가져오면서 제거
 
-            for(int i=0; i<4; i++){
-                Point np = new Point(tp.x+dirx[i], tp.y+diry[i], tp.cnt+1);
-                if(np.x>=0 && np.x < M && np.y>=0 && np.y<N
-                  && (box[np.y][np.x] == 0 || box[np.y][np.x]>tp.cnt+1)){
-                    box[np.y][np.x] = tp.cnt+1;
-                    stque.add(np);
+            for(int i = 0; i < 4; i++){
+                int nx = tp.x + dirx[i];
+                int ny = tp.y + diry[i];
+                
+                if(nx >= 0 && nx < M && ny >= 0 && ny < N
+                  && box[ny][nx] == 0){
+                    box[ny][nx] = tp.cnt + 1;
+                    stque.add(new Point(nx, ny, tp.cnt + 1));
                 }
             }
-
-            stque.remove();
         }
         return box;
     }
